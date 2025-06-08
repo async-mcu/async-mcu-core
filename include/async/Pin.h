cd5d28abae819610815da1a29eb341f349c33bbb
Lampusha <basstazz@yandex.ru>
2025-06-08 11:37:50 +0300

    upd analog write

--- include/async/Pin.h
+++ include/async/Pin.h
@@ -50,7 +50,7 @@ namespace async {
 
             ::digitalWrite(pin, state);
         }
-        void analogWrite(bool state) {
+        void analogWrite(int state) {
             if(mode != OUTPUT) {
                 setMode(OUTPUT);
             }
